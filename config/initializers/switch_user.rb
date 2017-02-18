383b3a5527f8d3687dc6b180dbb98666cb90e28c
Richard Huang <flyerhzm@gmail.com>
2017-02-18 10:21:49 +0800

    enable switch_user on production

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -3,8 +3,8 @@ SwitchUser.setup do |config|
   config.available_users = { user: -> { User.all }, admin: -> { Admin.all } }
   config.available_users_identifiers = { user: :id, admin: :id }
   config.available_users_names = { user: :email, admin: :email }
-  config.controller_guard = ->(current_user, request) { Rails.env.development? }
-  config.view_guard = ->(current_user, request) { Rails.env.development? }
+  config.controller_guard = ->(current_user, request) { true }
+  config.view_guard = ->(current_user, request) { true }
   config.redirect_path = ->(request, params) { params[:scope_identifier] =~ /^admin/ ? '/admin' : '/' }
   config.helper_with_guest = false
 end
